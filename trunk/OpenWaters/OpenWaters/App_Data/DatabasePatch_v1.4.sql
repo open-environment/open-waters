c97639b08445a197a1ad045b2327eaa6202ff0c5
my.open.environment@gmail.com <my.open.environment@gmail.com@82ac4711-7e6d-b3dd-0e02-65f0e4e2ef41>
2014-07-09 21:05:07 +0000

    Implemented County data synchronization

---
+++ trunk/OpenWaters/OpenWaters/App_Data/DatabasePatch_v1.4.sql
@@ -0,0 +1,5 @@
+﻿--run this script to apply changes for Open Waters v1.4 patch (upgrading from v1.3)
+
+INSERT INTO [OpenEnvironment].[dbo].[T_OE_APP_SETTINGS] ([SETTING_NAME],[SETTING_VALUE],[MODIFY_USERID],[MODIFY_DT])
+     VALUES ('Default State','OK','SYSTEM',GetDate());
+
