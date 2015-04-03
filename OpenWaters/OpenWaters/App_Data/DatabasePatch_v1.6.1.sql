332c47f697421bcbccf4f1b36c3fad01af27982a
my.open.environment <my.open.environment@gmail.com>
2015-04-03 18:43:19 +0000

    Version 1.6.1

--- OpenWaters/OpenWaters/App_Data/DatabasePatch_v1.6.1.sql
+++ OpenWaters/OpenWaters/App_Data/DatabasePatch_v1.6.1.sql
@@ -1,7 +1,7 @@
 ﻿/*
-Change Log
+Version 1.6.1 Change Log
 --------------------
-1. Improvements to Import Template Configuration Page: characteristic and unit drop-downs added
+1. Improvements to Crosstab Import Template Configuration Page: characteristic and unit drop-downs added
 2. Added new help page
-3. Updated Users/Installation Guide
+3. Updated Installation/Users Guide
 4. Fix import from cross tab when no Activity Type or Activity Media provided to show correct error message.
